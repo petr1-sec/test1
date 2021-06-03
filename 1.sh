216dff84efeb6c54377cb5698a4493c705e9e2e9
Пётр Первый ⟁ <72628056+petr1-sec@users.noreply.github.com>
2021-06-04 01:03:40 +0300

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -58,12 +58,100 @@ swapon /dev/sda3
 mount /dev/sda4 /mnt/home
 
 echo 'Выбор зеркал для загрузки. Ставим зеркало от Яндекс'
-#echo "Server = http://mirror.yandex.ru/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
+echo "Server = http://mirror.yandex.ru/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
 
-pacman -S reflector && reflector --verbose  -l 15 -p https --sort rate --save /etc/pacman.d/mirrorlist && pacman -Syyu
+#pacman -S reflector && reflector --verbose  -l 15 -p https --sort rate --save /etc/pacman.d/mirrorlist && pacman -Syyu
 
 echo 'Установка основных пакетов'
 pacstrap /mnt base base-devel linux linux-firmware nano dhcpcd netctl
 
 echo 'Настройка системы'
 genfstab -pU /mnt >> /mnt/etc/fstab
+
+#arch-chroot /mnt sh -c "$(curl -fsSL link.sh)"
+
+#read -p "Введите имя компьютера: " hostname
+#read -p "Введите имя пользователя: " username
+
+echo 'Прописываем имя компьютера'
+#echo $hostname > /etc/hostname
+echo 'PC1' > /etc/hostname
+ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
+
+echo 'Добавляем русскую локализация системы'
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+echo "ru_RU.UTF-8 UTF-8" >> /etc/locale.gen 
+
+echo 'Обновим текущую локаль системы'
+locale-gen
+
+echo 'Указываем язык системы'
+echo 'LANG="ru_RU.UTF-8"' > /etc/locale.conf
+
+echo 'Вписываем KEYMAP=ru FONT=cyr-sun16'
+echo 'KEYMAP=ru' >> /etc/vconsole.conf
+echo 'FONT=cyr-sun16' >> /etc/vconsole.conf
+
+echo 'Создадим загрузочный RAM диск'
+mkinitcpio -p linux
+
+echo 'Устанавливаем загрузчик'
+pacman -Syy
+pacman -S grub --noconfirm 
+grub-install /dev/sda
+
+echo 'Обновляем grub.cfg'
+grub-mkconfig -o /boot/grub/grub.cfg
+
+echo 'Ставим программу для Wi-fi'
+pacman -S dialog wpa_supplicant --noconfirm 
+
+echo 'Добавляем пользователя'
+#useradd -m -g users -G wheel -s /bin/bash $username
+useradd -m -g users -G wheel -s /bin/bash PC1-user1
+
+echo 'Создаем root пароль'
+passwd
+
+echo 'Устанавливаем пароль пользователя'
+#passwd $username
+passwd PC1-user1
+
+echo 'Устанавливаем SUDO'
+echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers
+
+echo 'Раскомментируем репозиторий multilib Для работы 32-битных приложений в 64-битной системе.'
+echo '[multilib]' >> /etc/pacman.conf
+echo 'Include = /etc/pacman.d/mirrorlist' >> /etc/pacman.conf
+pacman -Syy
+
+echo "Куда устанавливем Arch Linux на виртуальную машину?"
+read -p "1 - Да, 0 - Нет: " vm_setting
+echo 1
+if [[ $vm_setting == 0 ]]; then
+  gui_install="xorg-server xorg-drivers xorg-xinit"
+elif [[ $vm_setting == 1 ]]; then
+  gui_install="xorg-server xorg-drivers xorg-xinit virtualbox-guest-utils"
+fi
+
+echo 'Ставим иксы и драйвера'
+pacman -S $gui_install
+
+echo "Ставим XFCE"
+pacman -S xfce4 xfce4-goodies --noconfirm
+
+echo 'Cтавим DM'
+pacman -S lxdm --noconfirm
+systemctl enable lxdm
+
+echo 'Ставим шрифты'
+pacman -S ttf-liberation ttf-dejavu --noconfirm 
+
+echo 'Ставим сеть'
+pacman -S networkmanager network-manager-applet ppp --noconfirm
+
+echo 'Подключаем автозагрузку менеджера входа и интернет'
+systemctl enable NetworkManager
+
+echo 'wget git.io/arch3.sh && sh arch3.sh'
+exit
